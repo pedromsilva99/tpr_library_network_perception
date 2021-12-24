f9a58f54589ee218c932b1f2141becc51d0c8350
pedromsilva99 <pedromsilva99@ua.pt>
2021-12-24 04:11:22 +0000

    good bot

---
+++ bots/good_bot.py
@@ -0,0 +1,12 @@
+import time
+import requests
+import random
+while(1):
+    ls=["https://www.google.com/", "https://en.wikipedia.org/wiki/Main_Page", "https://www.youtube.com/", "https://twitter.com/", "https://www.sporcle.com/",
+    "https://vimeo.com/", "https://www.worldometers.info/", "https://stackoverflow.com/", "https://github.com/", "https://www.nytimes.com/"]
+    website = random.randrange(10)
+    sitedata=requests.get(ls[website])
+    print(ls[website])
+    numb = random.randrange(20)
+    print("Adormece " + str(numb) + " segundos")
+    time.sleep(numb)
