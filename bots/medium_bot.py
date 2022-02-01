b673e4cb918eac027ec1105914822bf382a81b9c
pedromsilva99 <pedromsilva99@ua.pt>
2022-02-01 13:56:00 +0000

    slight change

--- bots/medium_bot.py
+++ bots/medium_bot.py
@@ -5,6 +5,6 @@ while(1):
     #sitedata=requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     sitedata=requests.get("https://www.google.com/")
     #print(sitedata.content)
-    numb = random.randrange(20)
+    numb = random.randrange(90)
     print("Adormece " + str(numb) + " segundos")
     time.sleep(numb)
